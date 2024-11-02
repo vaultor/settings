72c8e5e16dbe7d0286dd23d58be395ecd4e6984b
Keith Leow <keith.leow@gmail.com>
2024-11-03 00:05:25 +0800

    e

--- docs/settings.js
+++ docs/settings.js
@@ -1,17 +1,18 @@
 const nextStylesheet = document.querySelector("link[data-n-g]").sheet;
 
-const bodyCSS = `
-  body {
-    background-color: #252727 !important;
-    font-family: Inter, Roboto !important;
-    font-feature-settings: "liga", "tnum" !important;
-  }
-`;
-
 const codeCSS = `
   code, kbd, pre, samp {
     font-family: "JetBrains Mono", SFMono-Regular, ui-monospace, monospace;
     font-size: 1.1em;
+  }
+`;
+
+// (200 3% 10%)
+const bodyCSS = `
+  body {
+    background-color: #191a1a !important;
+    font-family: Inter, Roboto !important;
+    font-feature-settings: "liga", "tnum" !important;
   }
 `;
 
