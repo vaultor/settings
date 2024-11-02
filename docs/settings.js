2cc9f7835b02a030af253839a1f03d969aadc344
Keith Leow <keith.leow@gmail.com>
2024-11-03 00:13:19 +0800

    f

--- docs/settings.js
+++ docs/settings.js
@@ -7,10 +7,10 @@ const codeCSS = `
   }
 `;
 
-// (200 3% 10%)
+// (200 3% 9%)
 const bodyCSS = `
   body {
-    background-color: #191a1a !important;
+    background-color: #161718 !important;
     font-family: Inter, Roboto !important;
     font-feature-settings: "liga", "tnum" !important;
   }
@@ -23,18 +23,18 @@ const sidebarCSS = `
   }
 `;
 
-// (200 2% 9%)
+// (200 2% 8%)
 const workspacebarCSS = `
   [data-element-id="workspace-bar"] {
-    background-color: #161717 !important;
+    background-color: #141515 !important;
     border-color: #303030 !important;
   }
 `;
 
 try {
   const length = nextStylesheet.cssRules.length;
-  nextStylesheet.insertRule(bodyCSS, length);
-  nextStylesheet.insertRule(codeCSS, length + 1);
+  nextStylesheet.insertRule(codeCSS, length);
+  nextStylesheet.insertRule(bodyCSS, length + 1);
   nextStylesheet.insertRule(sidebarCSS, length + 2);
   nextStylesheet.insertRule(workspacebarCSS, length + 3);
 } catch (error) {
