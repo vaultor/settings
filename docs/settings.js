8efa89f9b46972f1662634077db6a82a0f881f15
Keith Leow <keith.leow@gmail.com>
2024-11-02 22:57:33 +0800

    test

--- docs/settings.js
+++ docs/settings.js
@@ -1,10 +1,10 @@
-const workspace = document.querySelector('[data-element-id="workspace-bar"]');
-workspace.style.backgroundColor = "#161717"; // (200 2% 9%)
-workspace.style.borderColor = "#303030";
+// const workspace = document.querySelector('[data-element-id="workspace-bar"]');
+// workspace.style.backgroundColor = "#161717"; // (200 2% 9%)
+// workspace.style.borderColor = "#303030";
 
-document
-  .querySelector('[data-element-id="side-bar-background"]')
-  .style.setProperty("--sidebar-color", "#191a1a"); // (200 2% 10%)
+// document
+//   .querySelector('[data-element-id="side-bar-background"]')
+//   .style.setProperty("--sidebar-color", "#191a1a"); // (200 2% 10%)
 
 document.body.style.backgroundColor = "#252727"; //    (200 2% 15%)
 document.body.style.fontFamily = "Inter, Roboto";
@@ -16,6 +16,11 @@ const code = `
   code, kbd, pre, samp {
     font-family: "JetBrains Mono", SFMono-Regular, ui-monospace, monospace;
     font-size: 1.1em;
+  }
+
+
+  .bg-\[color\:var\(--sidebar-color\)\] {
+    background-color: #161717 !important;
   }
 `;
 
