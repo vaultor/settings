5fc801056976093cc081d66db42104dac6bfcc5c
Keith Leow <keith.leow@gmail.com>
2025-04-22 22:29:30 +0800

    fix cost typo

--- docs/settings.js
+++ docs/settings.js
@@ -33,7 +33,7 @@ const codeCSS = `
 const costCSS = `
   [data-tooltip-id="global"] span:nth-child(1) {
     margin-right: 8px;
-    font0size: 0.9em !important;
+    font-size: 0.9em !important;
     color: #ffc533 !important;
   }
 `;
