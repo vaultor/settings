6f5b386be1f250f1e98568e27b85845c4aa48f56
Keith Leow <keith.leow@gmail.com>
2025-04-22 22:24:26 +0800

    disable cost

--- docs/settings.js
+++ docs/settings.js
@@ -89,7 +89,7 @@ try {
   let length = nextStylesheet.cssRules.length;
   nextStylesheet.insertRule(hoverCSS, length++);
   nextStylesheet.insertRule(codeCSS, length++);
-  nextStylesheet.insertRule(costCSS, length++);
+  // nextStylesheet.insertRule(costCSS, length++);
   nextStylesheet.insertRule(bodyCSS, length++);
   nextStylesheet.insertRule(pane1CSS, length++);
   nextStylesheet.insertRule(pane2CSS, length++);
